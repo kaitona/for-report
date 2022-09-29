4d5ac96d4f86ec52077b8da6db104445065b1af5
kaitona <kaito146remi@gmail.com>
2022-09-29 22:03:28 +0900

    new file

--- 2022problemA.py
+++ 2022problemA.py
@@ -1,5 +1,21 @@
 import sys
-def inputData(){
-	data = sys.stdio
-	print(data)
-}
+
+def inputData():
+    data = sys.stdin
+    line_counter = 0
+    for line in data:
+        if line_counter%2 == 0:
+            days = int(line.replace("\n", ""))
+        else:
+            infectedList = line.replace("\n", "").split(" ")
+            print(peakCount(days, infectedList))
+        line_counter += 1
+
+def peakCount(days, infectedList):
+    count = 0
+    for i in range(1,days-1):
+        if (infectedList[i] > infectedList[i-1]) and  (infectedList[i] > infectedList[i+1]): count+= 1
+    return count
+
+inputData()
+
