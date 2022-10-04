f7684ac5c844d654041e898c465fcfbda2f49ea9
kaitona <kaito146remi@gmail.com>
2022-10-04 23:18:54 +0900

    new

--- 2020B/2022problemB.py
+++ 2020B/2022problemB.py
@@ -1,7 +1,22 @@
 import sys
 def inputData():
     data = sys.stdin
-    for text in data:
-        print(len(data))
+    formattingData = [line.replace("\n", "").split(" ") for line in data]
+    print(formattingData)
+    user = 0
+    patient = 0
+    id = 0
+    index = 0
 
+    while True:
+        if "".join(formattingData[index]) == "000":
+            print("end")
+            break
+        if len(formattingData[index]) == 3:
+            user = formattingData[index][0]
+            patient = formattingData[index][1]
+            id = formattingData[index][2]
+            print(user, patient, id)
+        index += 1
+        
 inputData()
