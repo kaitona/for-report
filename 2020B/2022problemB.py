d4732e4666a858b7997eb9518b81dff093d1542f
kaitona <kaito146remi@gmail.com>
2022-10-11 00:21:37 +0900

    complete

--- 2020B/2022problemB.py
+++ 2020B/2022problemB.py
@@ -2,20 +2,31 @@ import sys
 def inputData():
     infile = sys.stdin
     formattingData = [line.replace("\n", "").split(" ") for line in infile]
-    users = 0
-    data = 0
-    id = 0
-    index = 0
+    users = formattingData[0][0]
+    data = formattingData[0][1]
+    id = formattingData[0][2]
+    infectedList = []
+    infectedList.append(id)
+    index = 1
 
     while True:
         if "".join(formattingData[index]) == "000":
-            print("end")
+            print(len(infectedList))
             break
+
         if len(formattingData[index]) == 3:
+            print(len(infectedList))
             users = formattingData[index][0]
             data = formattingData[index][1]
             id = formattingData[index][2]
-            print(users, data, id)
+            infectedList = []
+            infectedList.append(id)
+            index += 1
+            continue
+
+        if ((formattingData[index][0] in infectedList and formattingData[index][1] not in infectedList) or 
+        (formattingData[index][1] in infectedList and formattingData[index][0] not in infectedList)):
+            infectedList.append(formattingData[index][1])
         index += 1
         
 inputData()
