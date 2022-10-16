32eaff1ae43ce510fdf9d8596c5ca8667aea4ff0
kaitona <kaito146remi@gmail.com>
2022-10-17 00:10:44 +0900

    new problem

--- 2020B/check.sh
+++ 2020B/check.sh
@@ -4,16 +4,16 @@ rm 2020B.out
 
 python3 2020problemB.py < ./B/B1 > ./B/B1.out
 diff ./B/B1.ans ./B/B1.out
-./B/B1.out
+rm ./B/B1.out
 
 python3 2020problemB.py < ./B/B2 > ./B/B2.out
 diff ./B/B2.ans ./B/B2.out
-./B/B2.out
+rm ./B/B2.out
 
 python3 2020problemB.py < ./B/B3 > ./B/B3.out
 diff ./B/B3.ans ./B/B3.out
-./B/B3.out
+rm ./B/B3.out
 
 python3 2020problemB.py < ./B/B4 > ./B/B4.out
 diff ./B/B4.ans ./B/B4.out
-./B/B4.out
+rm ./B/B4.out
---
+++ 2021A/check.sh
@@ -0,0 +1,19 @@
+python3 2021probleA.py < 2021A.in > 2021A.out
+diff 2021A.ans 2021A.out
+rm 2021A.out
+
+python3 2021problemA.py < ./A/A1 > ./A/A1.out
+diff ./A/A1.ans ./A/A1.out
+rm ./A/A1.out
+
+python3 2021problemA.py < ./A/A2 > ./A/A2.out
+diff ./A/A2.ans ./A/A2.out
+rm ./A/A2.out
+
+python3 2021problemA.py < ./A/A3 > ./A/A3.out
+diff ./A/A3.ans ./A/A3.out
+rm ./A/A3.out
+
+python3 2021problemA.py < ./A/A4 > ./A/A4.out
+diff ./A/A4.ans ./A/A4.out
+rm ./A/A4.out
